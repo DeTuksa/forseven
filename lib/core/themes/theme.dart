3f9e8ba4c5ed2d216884e98f521b52434ee081e8
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2025-03-29 22:06:46 +0000

    add: theme setup

--- lib/core/themes/theme.dart
+++ lib/core/themes/theme.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:forseven/core/utils/hex_color.dart';
 
@@ -10,7 +8,39 @@ class ForsevenTheme {
   static Color lightTextColor = HexColor("#f3f0ed");
   static Color darkTextColor = HexColor("#070a25");
 
+  static String fontFamily = "AmazonEmber";
+
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: false
+    fontFamily: fontFamily,
+    useMaterial3: false,
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: darkBlue,
+      surface: primary,
+      onSurface: darkTextColor,
+      onPrimary: lightTextColor,
+      error: Colors.red,
+      onError: Colors.white,
+      secondary: darkBlue,
+      onSecondary: lightTextColor
+    ),
+    brightness: Brightness.light,
+    textTheme: TextTheme(
+      displayLarge: TextStyle(fontSize: 57.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      displayMedium: TextStyle(fontSize: 45.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      displaySmall: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600, color: darkTextColor),
+      headlineMedium: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w600, color: darkTextColor),
+      headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500, color: darkTextColor),
+      titleLarge: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600, color: darkTextColor),
+      titleSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, color: darkTextColor),
+      labelLarge: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500, color: darkTextColor),
+      labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w500, color: darkTextColor),
+      labelSmall: TextStyle(fontSize: 11.0, fontWeight: FontWeight.w500, color: darkTextColor),
+      bodyLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: darkTextColor),
+      bodySmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: darkTextColor),
+    )
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forseven/core/themes/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Forseven EV',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ForsevenTheme.lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -42,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
