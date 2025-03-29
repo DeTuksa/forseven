d58d1f5a6ee071f666773db1729651acfadf683f
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2025-03-29 17:21:54 +0000

    add: added assets

--- lib/core/themes/theme.dart
+++ lib/core/themes/theme.dart
@@ -1,3 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:forseven/core/utils/hex_color.dart';
+
 class ForsevenTheme {
-  
+  static Color primary = HexColor("#fbf9f8");
+  static Color darkBlue = HexColor("#070a25");
+  static Color grey = HexColor("#d6d6d6");
+  static Color lightTextColor = HexColor("#f3f0ed");
+  static Color darkTextColor = HexColor("#070a25");
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: false
+  );
 }
